929ebf80a3a4b7d7bb346190a6be2251b40e38ea
Timothy Chen <tnachen@gmail.com>
2017-05-17 16:17:44 -0700

    Add function looping

---
+++ funcs/funcs.go
@@ -0,0 +1,41 @@
+package funcs
+
+import (
+	"errors"
+	"time"
+)
+
+func loopUntil(timeout time.Duration, stepTime time.Duration, loopFunc func() (bool, error)) error {
+	doneChan := make(chan bool, 1)
+	errChan := make(chan error, 1)
+	quitChan := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-quitChan:
+				return
+			default:
+				done, err := loopFunc()
+				if err != nil {
+					errChan <- err
+					return
+				} else if done {
+					doneChan <- true
+					return
+				}
+				time.Sleep(stepTime)
+			}
+		}
+	}()
+
+	select {
+	case <-doneChan:
+		return nil
+	case err := <-errChan:
+		return err
+	case <-time.After(timeout):
+		quitChan <- true
+		return errors.New("Time out waiting for loop to terminate")
+	}
+}
