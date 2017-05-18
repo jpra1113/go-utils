4870132a35e5087303a9118b9c1a7047b4f3045b
Timothy Chen <tnachen@gmail.com>
2017-05-17 19:29:27 -0700

    Add tests

--- funcs/funcs_test.go
+++ funcs/funcs_test.go
@@ -1,1 +1,31 @@
 package funcs
+
+import (
+	"errors"
+	"testing"
+	"time"
+)
+
+func TestLoopUntil(t *testing.T) {
+	count := 0
+	err := LoopUntil(time.Second*10, time.Millisecond*100, func() (bool, error) {
+		count += 1
+		if count < 5 {
+			return false, nil
+		}
+
+		return true, nil
+	})
+
+	if err != nil {
+		t.Error("Unable to finish loop before timeout: " + err.Error())
+	}
+
+	err = LoopUntil(time.Second*10, time.Second*1, func() (bool, error) {
+		return false, errors.New("Test error")
+	})
+
+	if err == nil {
+		t.Error("Expecting error from loop")
+	}
+}
